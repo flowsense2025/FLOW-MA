351fe56bfd32505130cf268dd2139359d484d2ce
Alex Chen <alexanderchen817@gmail.com>
2025-03-13 11:33:46 -0700

    added buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,31 @@
-import { Text, View, ImageBackground } from "react-native";
+import { Text, View, ImageBackground, StyleSheet, Button } from "react-native";
 import { useNavigation } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Index() {
     const navigation = useNavigation();
     navigation.setOptions({ headerShown: false });
   return (
-    <ImageBackground 
-        source={require("../assets/images/loginBackground.png")} 
-        style={{flex:1}}></ImageBackground>
-    
+    <SafeAreaProvider>
+        <ImageBackground 
+            source={require("../assets/images/V2.png")} 
+            style={{flex:1}}></ImageBackground>
+
+        <Button 
+            title="Login">
+        </Button>
+        <Button
+            title="Sign Up">
+        </Button>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+    loginButton: {
+        flex: 1,
+        justifyContent: 'center',
+        marginHorizontal: 16,
+        height: 85,
+    }
+});
