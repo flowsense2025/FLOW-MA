b47cfae369f1a28ba404a118b672db10a6e71942
Alex Chen <alexanderchen817@gmail.com>
2025-03-13 12:07:38 -0700

    added login and sign up buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,62 @@
-import { Text, View, ImageBackground, StyleSheet, Button } from "react-native";
+import { Text, View, ImageBackground, StyleSheet, TouchableOpacity, Button  } from "react-native";
 import { useNavigation } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import { useEffect } from "react";
 export default function Index() {
     const navigation = useNavigation();
-    navigation.setOptions({ headerShown: false });
+    useEffect(() => {
+        navigation.setOptions({ headerShown: false });
+    }, [navigation]);
   return (
     <SafeAreaProvider>
         <ImageBackground 
-            source={require("../assets/images/V2.png")} 
+            source={require("../assets/images/loginBackground.png")} 
             style={{flex:1}}></ImageBackground>
 
-        <Button 
-            title="Login">
-        </Button>
-        <Button
-            title="Sign Up">
-        </Button>
+        <View style={styles.loginButton}>
+            <Button  title="Login" color="white" onPress={() => {}} />
+        </View>
+        <View style={styles.signUpButton}>
+            <Button  title="Sign Up" color="white" onPress={() => {}} />
+        </View>
     </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-    loginButton: {
+    background: {
         flex: 1,
-        justifyContent: 'center',
-        marginHorizontal: 16,
-        height: 85,
-    }
+        width: "100%",
+        height: "100%",
+        position: "absolute", 
+      },
+     
+      loginButton: {
+        backgroundColor: "#DE6E56",
+        paddingVertical: 12,
+        paddingHorizontal: 24,
+        borderRadius: 15,
+        width: 160, 
+        alignItems: "center",
+        marginBottom: 15, // Adds spacing between buttons
+        position: "absolute",
+        left: 112,
+        bottom: 200,
+      },
+      signUpButton: {
+        left: 112,
+        bottom: 130,
+        backgroundColor: "#093593",
+        paddingVertical: 12,
+        paddingHorizontal: 24,
+        borderRadius: 15,
+        width: 160,
+        alignItems: "center",
+        position: "absolute",
+      },
+      buttonText: {
+        color: "white",
+        fontSize: 18,
+        fontWeight: "bold",
+      },
 });
