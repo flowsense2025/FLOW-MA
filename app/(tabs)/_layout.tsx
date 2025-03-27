9b5840c7489c64f2d164081ee86bd023be5ba076
Andrew La Grange <andrewj.lagrange@gmail.com>
2025-03-27 12:18:37 -0700

    Edited tab bar to be floating

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,37 @@
 import { Tabs } from 'expo-router'
 import { Ionicons } from "@expo/vector-icons"; // Import icons
 import Feather from '@expo/vector-icons/Feather'; // Import other icon
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 
 export default function Layout() {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                // Styles for the entire tab bar container
+                tabBarStyle: {
+                    backgroundColor: "#ffffff",
+                    borderTopColor: "transparent", // Removes top border
+                    elevation: 0, // Removes shadow on Android
+                    shadowOpacity: 0, // Removes shadow on iOS
+                    position: "absolute",
+                    bottom: 50, // Moves tab bar up from bottom of screen
+                    marginHorizontal: 36,
+
+                    //paddingBottom: 0,
+                    paddingTop: 0,
+                    borderRadius: 50, // Curves the corners of the tab bar
+                    width: 315,
+                    height: 56,
+                    
+                },
+                // Optionally style the label text
+                    tabBarLabelStyle: {
+                    fontSize: 12,
+                    fontWeight: "bold",
+                },
+            }}
+            >
             <Tabs.Screen 
                 name="stats" 
                 options={{ 
