a6b3ddedbe296b1689bcacd798440f3e82c7d976
Alex Chen <alexanderchen817@gmail.com>
2025-04-06 17:21:43 -0700

    add login on index front page

--- app/index.tsx
+++ app/index.tsx
@@ -1,56 +1,97 @@
-import { View, ImageBackground, StyleSheet, Button  } from "react-native";
-import { useNavigation, Link} from "expo-router";
+import { View, Text, TextInput, TouchableOpacity, ImageBackground, StyleSheet, Button } from "react-native";
+import { useNavigation, Link } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { router } from 'expo-router'
+import { auth } from '../firebaseConfig'
 export default function Index() {
     const navigation = useNavigation();
     useEffect(() => {
         navigation.setOptions({ headerShown: false });
     }, [navigation]);
-  return (
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const signIn = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, email, password)
+            if (user) router.replace('/(tabs)/home');
+        } catch (error: any) {
+            console.log(error)
+            alert('Sign in failed: ' + error.message);
+        }
+    }
+
+    const signUp = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, email, password)
+            if (user) router.replace('/(tabs)/home');
+        } catch (error: any) {
+            console.log(error)
+            alert('Sign in failed: ' + error.message);
+        }
+    }
+    return (
         // display login buttons
-      <SafeAreaProvider>
-        <ImageBackground 
-            source={require("../assets/images/LandingPage.png")} 
-            style={{flex:1}}>
-        </ImageBackground>
-        
-        <View style={styles.loginButton}>
-            <Link href="/(tabs)/home" asChild>
-                <Button title="Login" color="white"/>
-            </Link>
-        </View>
-        <View style={styles.signUpButton}>
-            <Button title="Sign Up" color="white"/>
-        </View>
-    </SafeAreaProvider>
-  );
+        <SafeAreaProvider style={{ flex: 1 }}>
+            <ImageBackground
+                source={require("../assets/images/LandingPage.png")}
+                style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Email"
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Password"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                    />
+
+                    <TouchableOpacity style={styles.loginButton} onPress={signIn}>
+                        <Text style={styles.text}>Login</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.signUpButton} onPress={signUp}>
+                        <Text style={styles.text}>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
     background: {
-        flex: 1,
-        width: "100%",
-        height: "100%",
-        position: "absolute", 
-      },
-     
-      loginButton: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+
+    loginButton: {
         backgroundColor: "#DE6E56",
         paddingVertical: 12,
         paddingHorizontal: 24,
         borderRadius: 15,
-        width: 160, 
+        width: 160,
         alignItems: "center",
         marginBottom: 15, // Adds spacing between buttons
         position: "absolute",
-        left: 112,
-        bottom: 200,
-      },
-      signUpButton: {
-        left: 112,
-        bottom: 130,
+        left: 115,
+        bottom: 150,
+    },
+    signUpButton: {
+        left: 115,
+        bottom: 100,
         backgroundColor: "#093593",
         paddingVertical: 12,
         paddingHorizontal: 24,
@@ -58,10 +99,23 @@ const styles = StyleSheet.create({
         width: 160,
         alignItems: "center",
         position: "absolute",
-      },
-      buttonText: {
-        color: "white",
-        fontSize: 18,
-        fontWeight: "bold",
-      },
+    },
+    text: {
+        color: '#FFFFFF', // Maintained white for clear visibility
+        fontSize: 17, // Slightly larger for emphasis
+    },
+    textInput: {
+        height: 50,
+        width: 180,
+        backgroundColor: '#FFFFFF',
+        borderColor: '#E8EAF6',
+        borderWidth: 2,
+        borderRadius: 15,
+        marginVertical: 10,
+        paddingHorizontal: 25,
+        fontSize: 16,
+        color: '#3C4858',
+        bottom: -470,
+        left: 110
+    },
 });
